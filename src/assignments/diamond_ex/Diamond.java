89a005b619da2c18763b0923c67bb17201226158
rachelsmithcode <Rachelcodesmith@outlook.com>
2016-09-14 15:14:12 +0100

    set up for Diamond ex done, first test passing - print one *

---
+++ src/assignments/diamond_ex/Diamond.java
@@ -0,0 +1,13 @@
+package assignments.diamond_ex;
+
+/**
+ * Created by RachelSmith on 14/09/2016.
+ */
+public class Diamond {
+
+    public void horizontalLine() {
+
+        String line = "*";
+        System.out.println(line);
+    }
+}
---
+++ tests/assignments/diamond_ex/DiamondTests.java
@@ -0,0 +1,38 @@
+package assignments.diamond_ex;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by RachelSmith on 14/09/2016.
+ */
+public class DiamondTests {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+    }
+
+    @Test
+    public void printsAStar() throws Exception {
+        new Diamond().horizontalLine();
+        assertEquals("*\n", outContent.toString());
+    }
+
+
+}
